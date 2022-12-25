0c6db7ef97892892474c626fd9e3238a63bde6a2
IvanStoilovski <ivanstoilovski1@gmail.com>
2022-12-25 18:44:24 +0100

    Adding a feature to locate the closest pharmacy

--- Domashna 3/tech prototype/pharmacies-master/pharmacies-master/src/main/java/com/example/pharmacies/bootstrap/DataInsert.java
+++ Domashna 3/tech prototype/pharmacies-master/pharmacies-master/src/main/java/com/example/pharmacies/bootstrap/DataInsert.java
@@ -19,12 +19,12 @@ public class DataInsert {
 
     @PostConstruct
     public void init() throws FileNotFoundException {
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\38970\\Desktop\\DIANS\\Domasna-DIANS\\Domashna 3\\tech prototype\\pharmacies-master\\final.txt"));
-      // String name, String location, String workingTime, Double lon, Double lat, String phoneNumber, String city, String website
-        br.lines().forEach(i->{
-                   String [] line = i.split("\\|");
-                   pharmacyRepository.save(new Pharmacy(line[2], line[3], line[7],
-                           Double.parseDouble(line[0]), Double.parseDouble(line[1]), line[5], line[4], line[6]));
-               });
-   }
+//        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\38970\\Desktop\\DIANS\\Domasna-DIANS\\Domashna 3\\tech prototype\\pharmacies-master\\final.txt"));
+//      // String name, String location, String workingTime, Double lon, Double lat, String phoneNumber, String city, String website
+//        br.lines().forEach(i->{
+//                   String [] line = i.split("\\|");
+//                   pharmacyRepository.save(new Pharmacy(line[2], line[3], line[7],
+//                           Double.parseDouble(line[0]), Double.parseDouble(line[1]), line[5], line[4], line[6]));
+//               });
+  }
 }
